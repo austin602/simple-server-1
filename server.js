0a79735e7b3a8699f8cf0456df3657510be351a7
Ron Bravo <ronbravo1701@gmail.com>
2016-11-21 11:22:38 -0700

    Trying to debug issue with handlebar templating engine.

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ var express = require ('express');
 // Create the express server app.
 var server = express ();
 
+
+// view engine setup
+server.set('views', __dirname + '/views');
+
+var exphbs = require('express-handlebars');
+server.engine('.hbs', exphbs({
+    defaultLayout: 'index',
+    extname: '.hbs'
+}));
+server.set('view engine', '.hbs');
+
+
+// var handlebars = require ('express-handlebars');
+// server.engine ('.hbs', handlebars ({
+//     defaultLayout: 'index',
+//     layoutsDir: 'templates',
+//     extname: '.hbs'
+// }));
+//
+// server.set ('view-engine', '.hbs');
+// // server.set ('views', __dirname + '/templates');
+
 // Set the port that our server will run on.
 var port = 3000;
 
@@ -18,4 +40,21 @@ server.listen (port, function (error) {
         // No errors found the server is good to go.
         console.log ('- The server has successfully started on port: ' + port);
     }
+});
+
+
+
+//--------------------------------------------
+// Set the url routes that the server can use.
+
+// Home or root route.
+server.get ('/', function (request, response) {
+    // NOTE: The request object contains information about
+    // the user's request (ex: their ip, headers, cookies, and params)
+
+    // NOTE: The response object is used to send responses
+    // back to the user who made the request.
+
+    // response.send ('<h1>Hello World!</h1>');
+    response.render ('home');
 });
