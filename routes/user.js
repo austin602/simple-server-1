3e6a419df4c759409a4371129622ec77477e260c
Ron Bravo <ronbravo1701@gmail.com>
2016-11-22 16:32:39 -0700

    Allowing for user to login and be registered.

--- routes/user.js
+++ routes/user.js
@@ -11,14 +11,74 @@ router.get ('/login', function (request, response) {
 });
 
 router.post ('/login', function (request, response) {
-    response.send ('You have now posted to the login route.');
+    // response.send ('You have now posted to the login route.');
 
     // Run a query to pull the user from the database
     // using the 'username' field sent down by the
     // post data.
-    db.collection ('users').findOne ({ username: request.body.username }, {}, function (error, result) {
-        console.log ('This is the result of the query: ', result);
-    });
+    db.collection ('users').findOne (
+        // The filter or fields to search by.
+        {
+            username: request.body.username,
+            password: request.body.password
+        },
+
+        // Additional query options.
+        {},
+
+        // Callback function.
+        function (error, result) {
+
+            // Check for errors.
+            if (error) {
+                console.error ('*** ERROR: Problem finding the user.');
+                console.error (error);
+                response.send ('There was an error with the page.');
+            }
+            else if (!result) {
+                // The query was run but did NOT find a matching
+                // object
+                response.send ('Your username or password is NOT correct.');
+            }
+            else {
+                // The query was run and DID find a matching object.
+                // response.send ('Found the user by the name: ' + result.username);
+                response.redirect ('/post');
+            }
+
+            // console.log ('This is the result of the query: ', result);
+        }
+    );
+});
+
+
+router.get ('/register', function (request, response) {
+    response.render ('register');
+});
+
+router.post ('/register', function (request, response) {
+
+    db.collection ('users').insertOne (
+        // Data to save to collection.
+        {
+            username: request.body.username,
+            password: request.body.password,
+            email: request.body.email
+        },
+
+        // The callback function to run once the save is complete.
+        function (error, result) {
+            // Check for an error.
+            if (error) {
+                console.error ('*** ERROR: Unable to register user.');
+                response.send ('Server error, unable to register user.');
+            }
+            else {
+                // Redirect to the login page.
+                response.redirect ('/user/login');
+            }
+        }
+    )
 });
 
 
